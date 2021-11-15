51f832d65d63d2085447326e54a56402b02853af
yva <68041302+yvaaa@users.noreply.github.com>
2021-11-14 20:33:21 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,3 +1,16 @@
+--[[
+
+game list:
+
+Jetpack Jumpers : https://www.roblox.com/games/7444263453/Jetpack-Jumpers-BETA
+
+Lawn Mowing Simulator : https://www.roblox.com/games/4528239858/x20-MONEY-LAWN-MOWING-SIMULATOR
+
+more will be added soon
+
+]]--
+
+
 if game.PlaceId == 4528239858 then
 loadstring(game:httpget("https://raw.githubusercontent.com/yvaaa/sleepers-hub/main/main/lawnmowingsimulator.lua"))()
 elseif game.PlaceId == 7444263453 then
