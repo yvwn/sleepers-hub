79b95dbe505d0ef541bf7c08806e8ede40139abf
yva <68041302+yvaaa@users.noreply.github.com>
2021-11-14 20:31:51 -0700

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 if game.PlaceId == 4528239858 then
-
+loadstring(game:httpget("https://raw.githubusercontent.com/yvaaa/sleepers-hub/main/main/lawnmowingsimulator.lua"))()
 elseif game.PlaceId == 7444263453 then
-
+loadstring(game:httpget("https://raw.githubusercontent.com/yvaaa/sleepers-hub/main/main/jetpackjumpers.lua"))()
 end
